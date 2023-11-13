c2d208462a3c8226f78652fb40c61b1790d09893
talha <talhamahmood2003@gmail.com>
2023-11-12 22:41:03 -0500

    Implemented InstructorController

--- Server/Controllers/UD/InstructorController.cs
+++ Server/Controllers/UD/InstructorController.cs
@@ -1,11 +1,230 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OCTOBER.EF.Data;
+using OCTOBER.EF.Models;
+using OCTOBER.Shared;
+using Telerik.DataSource;
+using Telerik.DataSource.Extensions;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Linq.Dynamic.Core;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis;
+using AutoMapper;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+using static System.Collections.Specialized.BitVector32;
+using static Duende.IdentityServer.Models.IdentityResources;
+
 namespace OCTOBER.Server.Controllers.UD
 {
-	public class InstructorController
-	{
-		public InstructorController()
-		{
-		}
-	}
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class InstructorController : BaseController, GenericRestController<InstructorDTO>
+    {
+        public InstructorController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+        [HttpDelete]
+        [Route("Delete/{SchoolId}/{InstructorId}")]
+
+        public async Task<IActionResult> Delete(int SchoolId, int InstructorId)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.Instructors.Where(x => x.SchoolId == SchoolId)
+                    .Where(x => x.InstructorId == InstructorId)
+                    .FirstOrDefaultAsync();
+
+                if (itm != null)
+                {
+                    _context.Instructors.Remove(itm);
+                }
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpGet]
+        [Route("Get")]
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var result = await _context.Instructors.Select(sp => new InstructorDTO
+                {
+                    SchoolId = sp.SchoolId,
+                    InstructorId = sp.InstructorId,
+                    Salutation = sp.Salutation,
+                    FirstName = sp.FirstName,
+                    LastName = sp.LastName,
+                    StreetAddress = sp.StreetAddress,
+                    Zip = sp.Zip,
+                    Phone = sp.Phone,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate,
+                })
+                .ToListAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpGet]
+        [Route("Get/{SchoolId}/{InstructorId}")]
+
+        public async Task<IActionResult> Get(int SchoolId, int InstructorId)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                InstructorDTO? result = await _context
+                    .Instructors
+                    .Where(x => x.SchoolId == SchoolId)
+                    .Where(x => x.InstructorId == InstructorId)
+                    .Select(sp => new InstructorDTO
+                     {
+                        SchoolId = sp.SchoolId,
+                        InstructorId = sp.InstructorId,
+                        Salutation = sp.Salutation,
+                        FirstName = sp.FirstName,
+                        LastName = sp.LastName,
+                        StreetAddress = sp.StreetAddress,
+                        Zip = sp.Zip,
+                        Phone = sp.Phone,
+                        CreatedBy = sp.CreatedBy,
+                        CreatedDate = sp.CreatedDate,
+                        ModifiedBy = sp.ModifiedBy,
+                        ModifiedDate = sp.ModifiedDate,
+                    })
+                .SingleOrDefaultAsync();
+
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpPost]
+        [Route("Post")]
+
+        public async Task<IActionResult> Post([FromBody] InstructorDTO _InstructorDTO)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.Instructors.Where(x => x.SchoolId == _InstructorDTO.SchoolId)
+                    .Where(x => x.InstructorId == _InstructorDTO.InstructorId)
+                    .FirstOrDefaultAsync();
+
+                if (itm == null)
+                {
+                    Instructor i = new Instructor
+                    {
+                        SchoolId = _InstructorDTO.SchoolId,
+                        InstructorId = _InstructorDTO.InstructorId,
+                        Salutation = _InstructorDTO.Salutation,
+                        FirstName = _InstructorDTO.FirstName,
+                        LastName = _InstructorDTO.LastName,
+                        StreetAddress = _InstructorDTO.StreetAddress,
+                        Zip = _InstructorDTO.Zip,
+                        Phone = _InstructorDTO.Phone,
+                    };
+                    _context.Instructors.Add(i);
+                    await _context.SaveChangesAsync();
+                    await _context.Database.CommitTransactionAsync();
+                }
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpPut]
+        [Route("Put")]
+
+        public async Task<IActionResult> Put([FromBody] InstructorDTO _InstructorDTO)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.Instructors.Where(x => x.SchoolId == _InstructorDTO.SchoolId)
+                    .Where(x => x.InstructorId == _InstructorDTO.InstructorId)
+                    .FirstOrDefaultAsync();
+
+                itm.SchoolId = _InstructorDTO.SchoolId;
+                itm.InstructorId = _InstructorDTO.InstructorId;
+                itm.Salutation = _InstructorDTO.Salutation;
+                itm.FirstName = _InstructorDTO.FirstName;
+                itm.LastName = _InstructorDTO.LastName;
+                itm.StreetAddress = _InstructorDTO.StreetAddress;
+                itm.Zip = _InstructorDTO.Zip;
+                itm.Phone = _InstructorDTO.Phone;
+
+                _context.Instructors.Update(itm);
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        //Implemented above with the arguments
+        public Task<IActionResult> Delete(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IActionResult> Get(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
 
---
+++ Shared/DTO/InstructorDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OCTOBER.Shared.DTO
+{
+	public class InstructorDTO
+	{
+        [Precision(8)]
+        public int SchoolId { get; set; }
+        [Precision(8)]
+        public int InstructorId { get; set; }
+        [StringLength(5)]
+        [Unicode(false)]
+        public string? Salutation { get; set; }
+        [StringLength(25)]
+        [Unicode(false)]
+        public string FirstName { get; set; } = null!;
+        [StringLength(25)]
+        [Unicode(false)]
+        public string LastName { get; set; } = null!;
+        [StringLength(50)]
+        [Unicode(false)]
+        public string StreetAddress { get; set; } = null!;
+        [StringLength(5)]
+        [Unicode(false)]
+        public string Zip { get; set; } = null!;
+        [StringLength(15)]
+        [Unicode(false)]
+        public string? Phone { get; set; }
+        [StringLength(30)]
+        [Unicode(false)]
+        public string CreatedBy { get; set; } = null!;
+        public DateTime CreatedDate { get; set; }
+        [StringLength(30)]
+        [Unicode(false)]
+        public string ModifiedBy { get; set; } = null!;
+        public DateTime ModifiedDate { get; set; }
+    }
+}
+
